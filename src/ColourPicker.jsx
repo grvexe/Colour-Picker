56305c899df87f69cddf39d179dba4fa7091bd0c
Gaurav Yadav <gauravyadav8646@gmail.com>
2024-02-25 15:20:29 +0530

    Add code to select and display colour

--- src/ColourPicker.jsx
+++ src/ColourPicker.jsx
@@ -1,6 +1,25 @@
+import React, {useState} from 'react';
 
 function ColourPicker(){
 
+    const [colour, setColour] = useState("#FFFFFF");
+
+    function colourChange(event){
+        setColour(event.target.value);
+    }
+
+    return(
+        <div className="container">
+            <div className="wrapper">
+                <h1>Colour Picker</h1>
+                <div className="colour-display" style={{backgroundColor: colour}}>
+                    <p>Selected Colour: {colour}</p>
+                </div>
+                <label>Select a colour: </label>
+                <input type="color" value={colour} onChange={colourChange}/>
+            </div>
+        </div>
+    )
 }
 
 export default ColourPicker
